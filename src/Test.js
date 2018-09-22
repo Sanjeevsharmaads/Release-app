3f15203efd4495d3857811643ce232f59b139b01
sanjeev sharma <sanjeev.sharma@affluencesolutions.com>
2018-09-22 17:09:31 +0530

    new file

---
+++ src/Test.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class Test extends Component {
+  render() {
+    return (
+      <div>
+        <p>This is release-app test class </p>
+      </div>
+    );
+  }
+}
+
+export default Test;
